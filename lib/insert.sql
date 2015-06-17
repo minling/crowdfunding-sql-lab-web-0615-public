cf870f667a4e16c81148dcd38e60c978bd535b5d
minling <mzhao725@gmail.com>
2015-06-17 17:34:07 -0400

    done

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,78 @@
 -- your code here
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES 
+('robot', 'tech', 4000, '6-20', '12-20'),
+('foldio', 'tech', 3000, '6-21', '8-30'),
+('chip', 'tech', 50000, '6-20', '8-31'),
+('Miito', 'science', 167000, '6-30', '8-31'),
+('Phree', 'tech', 100000, '6-24', '9-4'),
+('Bruno', 'kitchen', 50000, '7-3', '10-3'),
+('wake', 'tech', 100000, '4-24', '9-9'),
+('Makey Makey go', 'food', 10000, '5-1', '7-24'),
+('Garbage pick up', 'environment', 3000000, '4-21', '12-31'),
+('outerspace', 'environment', 1234567, '4-2', '11-11');
+
+INSERT INTO users (name, age) 
+VALUES
+('Melinda', 25),
+('Gary', 45),
+('Priscilla', 32),
+('Maybelline', 30),
+('Katarina', 24),
+('Aatrox', 30),
+('Ahri', 12),
+('Annie', 6),
+('Cassiopeia', 25),
+('Evelynn', 29),
+('Garen', 34),
+('Ezreal', 34),
+('Karma', 23),
+('LeBlanc', 45),
+('Lee Sin', 24),
+('Lux', 16),
+('Olaf', 49),
+('Nami', 23),
+('Morgana', 34),
+('Taric', 28);
+
+INSERT INTO pledges (amount)
+VALUES
+(1),
+(399),
+(49380),
+(2334),
+(23),
+(2343),
+(24599),
+(4775),
+(736),
+(72),
+(65839),
+(849),
+(500),
+(99),
+(24),
+(4995),
+(200),
+(4969),
+(2050),
+(5694),
+(399),
+(2300),
+(548),
+(37473),
+(638),
+(190),
+(48),
+(10020),
+(542),
+(3874);
+
+
+
+
+
+
+
+
+
